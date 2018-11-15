673afb1729cbe3dd2227801c762b328a58ec6425
QYDFrank <249867064@qq.com>
2018-11-15 15:50:15 +0800

    second commit from leona

--- shoppingproject/src/main/java/Demo.java
+++ shoppingproject/src/main/java/Demo.java
@@ -3,5 +3,6 @@ public class Demo {
     public static void main(String[] args){
         System.out.print("first git project");
         System.out.print("commit from leona");
+        System.out.print("second commit from leona");
     }
 }
